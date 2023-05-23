db41337cd8d1b1f8f036c360691075d7ad32f94f
bakjoul <bakjoul@gmail.com>
2023-05-23 20:06:51 +0200

    Created list fragment

---
+++ app/src/main/java/com/bakjoul/realestatemanager/ui/list/ListFragment.kt
@@ -0,0 +1,25 @@
+package com.bakjoul.realestatemanager.ui.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bakjoul.realestatemanager.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        _binding = null
+    }
+}
