0d73ec4052008f3d3559b54703688f430fc58941
bakjoul <bakjoul@gmail.com>
2023-05-23 20:13:34 +0200

    Added Hilt

--- app/src/main/java/com/bakjoul/realestatemanager/ui/main/MainActivity.kt
+++ app/src/main/java/com/bakjoul/realestatemanager/ui/main/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bakjoul.realestatemanager.databinding.MainActivityBinding
 import com.bakjoul.realestatemanager.ui.list.ListFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private var _binding: MainActivityBinding? = null
