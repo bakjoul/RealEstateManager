e7b555a3e450c33e0d0563391046c8995ca18d39
bakjoul <bakjoul@gmail.com>
2023-05-29 13:58:32 +0200

    Created PropertyItemViewState and PropertyAdapter

---
+++ app/src/main/java/com/bakjoul/realestatemanager/ui/list/PropertyAdapter.kt
@@ -0,0 +1,35 @@
+package com.bakjoul.realestatemanager.ui.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bakjoul.realestatemanager.databinding.FragmentListItemBinding
+
+class PropertyAdapter : ListAdapter<PropertyItemViewState, PropertyAdapter.ViewHolder>(PropertyDiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        FragmentListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(private val binding: FragmentListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: PropertyItemViewState) {
+            // TODO photo
+            binding.listItemType.text = item.type
+            binding.listItemCity.text = item.city
+            binding.listItemPrice.text = item.price
+        }
+    }
+
+    object PropertyDiffCallback : DiffUtil.ItemCallback<PropertyItemViewState>() {
+        override fun areItemsTheSame(oldItem: PropertyItemViewState, newItem: PropertyItemViewState): Boolean = oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: PropertyItemViewState, newItem: PropertyItemViewState): Boolean = oldItem == newItem
+    }
+}
---
+++ app/src/main/java/com/bakjoul/realestatemanager/ui/list/PropertyItemViewState.kt
@@ -0,0 +1,8 @@
+package com.bakjoul.realestatemanager.ui.list
+
+data class PropertyItemViewState (
+    val id: Long,
+    val type: String,
+    val city: String,
+    val price: String
+)
