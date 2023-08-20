159bf6a906a6bf746b4ebc1e3a7e6aed35c565ca
bakjoul <joul_77@hotmail.com>
2023-08-21 01:10:11 +0200

    Added slide in/out animations for add property fragment

--- app/src/main/java/com/bakjoul/realestatemanager/ui/main/MainActivity.kt
+++ app/src/main/java/com/bakjoul/realestatemanager/ui/main/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -13,8 +14,8 @@ import androidx.core.view.MenuProvider
 import com.bakjoul.realestatemanager.R
 import com.bakjoul.realestatemanager.databinding.ActivityMainBinding
 import com.bakjoul.realestatemanager.ui.add.AddPropertyFragment
-import com.bakjoul.realestatemanager.ui.details.activity.DetailsActivity
 import com.bakjoul.realestatemanager.ui.details.DetailsFragment
+import com.bakjoul.realestatemanager.ui.details.activity.DetailsActivity
 import com.bakjoul.realestatemanager.ui.dispatcher.DispatcherActivity
 import com.bakjoul.realestatemanager.ui.list.PropertyListFragment
 import com.bakjoul.realestatemanager.ui.photos.PhotosFragment
@@ -27,6 +28,11 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private companion object {
+        private const val PHOTOS_DIALOG_TAG = "PhotosDialogFragment"
+        private const val ADD_PROPERTY_FRAGMENT_TAG = "AddPropertyFragment"
+    }
+
     private val binding by viewBinding { ActivityMainBinding.inflate(it) }
     private val viewModel by viewModels<MainViewModel>()
 
@@ -36,6 +42,7 @@ class MainActivity : AppCompatActivity() {
 
         setToolbar()
         setNavigationView()
+        handleOnBackPressed()
 
         val containerDetailsId = binding.mainFrameLayoutContainerDetails?.id
         // Empty fragment if in tablet mode
@@ -58,17 +65,19 @@ class MainActivity : AppCompatActivity() {
 
                 MainViewAction.ShowPhotosDialog -> {
                     Log.d("test", "main: dialog emit")
-                    val existingDialog = supportFragmentManager.findFragmentByTag("PhotosDialogFragment") as? PhotosFragment
+                    val existingDialog = supportFragmentManager.findFragmentByTag(PHOTOS_DIALOG_TAG) as? PhotosFragment
                     if (existingDialog == null) {
                         val dialog = PhotosFragment()
-                        dialog.show(supportFragmentManager, "PhotosDialogFragment")
+                        dialog.show(supportFragmentManager, PHOTOS_DIALOG_TAG)
                     }
                 }
 
                 MainViewAction.ShowAddProperty -> {
-                    if (containerDetailsId != null) {
+                    val existingDialog = supportFragmentManager.findFragmentByTag(ADD_PROPERTY_FRAGMENT_TAG) as? AddPropertyFragment
+                    if (containerDetailsId != null && existingDialog == null) {
                         supportFragmentManager.beginTransaction()
-                            .add(containerDetailsId, AddPropertyFragment())
+                            .setCustomAnimations(R.anim.slide_in_bottom, 0)
+                            .add(containerDetailsId, AddPropertyFragment(), ADD_PROPERTY_FRAGMENT_TAG)
                             .addToBackStack(null)
                             .commit()
                     }
@@ -76,9 +85,10 @@ class MainActivity : AppCompatActivity() {
 
                 MainViewAction.CloseAddProperty -> {
                     if (containerDetailsId != null) {
-                        val addPropertyFragment = supportFragmentManager.findFragmentById(containerDetailsId)
+                        val addPropertyFragment = supportFragmentManager.findFragmentByTag(ADD_PROPERTY_FRAGMENT_TAG)
                         if (addPropertyFragment != null) {
                             supportFragmentManager.beginTransaction()
+                                .setCustomAnimations(0, R.anim.slide_out_bottom)
                                 .remove(addPropertyFragment)
                                 .commitNow()
                         }
@@ -119,6 +129,24 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
+
+    private fun handleOnBackPressed() {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                val addPropertyFragment =
+                    supportFragmentManager.findFragmentByTag(ADD_PROPERTY_FRAGMENT_TAG)
+
+                if (addPropertyFragment != null) {
+                    supportFragmentManager.beginTransaction()
+                        .setCustomAnimations(0, R.anim.slide_out_bottom)
+                        .remove(addPropertyFragment)
+                        .commit()
+                } else {
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
     }
 
     private fun setMenu() {
